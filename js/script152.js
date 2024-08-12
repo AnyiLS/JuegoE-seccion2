a78b4e716f0d0ef1067e77d785d78c01651d58ef
AnyiLS <tdprogramacioneml5@gmail.com>
2024-08-12 17:39:08 -0500

    first butom animate

---
+++ js/script152.js
@@ -0,0 +1,65 @@
+$(document).ready(function () {
+	// modal 1
+	$('.openModal').on('click', () => {
+		$('.modal').css('display', 'block')
+	})
+	// cerra modal
+	$('.closeModal').on('click', () => {
+		$('.modal').css('display', 'none')
+	})
+
+	// modal 1
+	$('.openModal1').on('click', () => {
+		$('.modal1').css('display', 'block')
+	})
+	// cerra modal
+	$('.closeModal1').on('click', () => {
+		$('.modal1').css('display', 'none')
+	})
+
+	// modal 2
+	$('.openModal2').on('click', () => {
+		$('.modal2').css('display', 'block')
+	})
+	// cerra modal
+	$('.closeModal2').on('click', () => {
+		$('.modal2').css('display', 'none')
+	})
+
+	// modal 3
+	$('.openModal3').on('click', () => {
+		$('.modal3').css('display', 'block')
+	})
+	// cerra modal
+	$('.closeModal3').on('click', () => {
+		$('.modal3').css('display', 'none')
+	})
+
+	// modal 4
+	$('.openModal4').on('click', () => {
+		$('.modal4').css('display', 'block')
+	})
+	// cerra modal
+	$('.closeModal4').on('click', () => {
+		$('.modal4').css('display', 'none')
+	})
+
+	// modal 5
+	$('.openModal5').on('click', () => {
+		$('.modal5').css('display', 'block')
+	})
+	// cerra modal
+	$('.closeModal5').on('click', () => {
+		$('.modal5').css('display', 'none')
+	})
+
+	// modal 5
+	$('.openModal6').on('click', () => {
+		$('.modal6').css('display', 'block')
+	})
+	// cerra modal
+	$('.closeModal6').on('click', () => {
+		$('.modal6').css('display', 'none')
+	})
+
+})
